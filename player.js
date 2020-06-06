5cb065f71fb42c122bc2a678b1e927dae63cc818
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-06-06 17:59:54 -0600

    Add conditional to playcard function

--- player.js
+++ player.js
@@ -2,13 +2,17 @@ class Player {
   constructor(playerNum) {
     this.id = playerNum;
     this.wins = 0;
-    this.myTurn = true;
     this.hand = [];
   }
 
   playCard() {
     currentGame.middleDeck.push(this.hand.pop());
     console.log(currentGame.middleDeck[currentGame.middleDeck.length -1]);
+    if (this.id === 1) {
+      currentGame.currentTurn = 2;
+    } else if (this.id ===2) {
+      currentGame.currentTurn = 1;
+    }
   }
 
   saveWinsToStorage() {
