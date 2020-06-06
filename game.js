e5bd483ce48037e41bd17cd01809d9db8e19d2fb
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-06-06 16:36:13 -0600

    Begin game class

---
+++ game.js
@@ -0,0 +1,8 @@
+class Game {
+  constructor() {
+    this.player1 = new Player(1);
+    this.player2 = new Player(2);
+    this.middleDeck = [];
+    this.allCards = ["blue-01", "blue-02", "blue-03", "blue-04", "blue-05", "blue-06", "blue-07"]
+  }
+}
