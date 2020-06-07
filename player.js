76ba4105b433d431860462a3275942784077f223
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-06-06 19:26:57 -0600

    Edit playcard so player can only playcard on their turn

--- player.js
+++ player.js
@@ -6,11 +6,19 @@ class Player {
   }
 
   playCard() {
-    currentGame.middleDeck.push(this.hand.pop());
-    console.log(currentGame.middleDeck[currentGame.middleDeck.length -1]);
+    if (currentGame.currentTurn === this.id) {
+      currentGame.middleDeck.push(this.hand.pop());
+      console.log(currentGame.middleDeck[currentGame.middleDeck.length -1]);
+      this.takeTurns();
+    } else {
+      console.log(`It is player ${currentGame.currentTurn}'s turn`)
+    }
+  }
+
+  takeTurns() {
     if (this.id === 1) {
       currentGame.currentTurn = 2;
-    } else if (this.id ===2) {
+    } else if (this.id === 2) {
       currentGame.currentTurn = 1;
     }
   }
